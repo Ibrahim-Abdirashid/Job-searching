4529664562ebfe5e405f24d6fc7c22e060924429
ibrahim-abdirashid <neeykaraamo@gmail.com>
2025-07-31 15:33:13 +0300

    resolve

--- js/home.js
+++ js/home.js
@@ -4,29 +4,28 @@ async function loadJobs() {
   const response = await fetch("./jobs.json");
   jobs = await response.json();
   localStorage.setItme("jobs", json.stringify(jobs));
-
 }
 
 function searchJobs() {
   const query = document.getElementById("searchBox").value.toLowerCase();
-  const savedJobs = JSON.parse(localStorage.getItem("jobs"))|| [];
-  const results = savedJobs.filter((job) => job.title.toLowerCase().includes(query));
-
+  const savedJobs = JSON.parse(localStorage.getItem("jobs")) || [];
+  const results = savedJobs.filter((job) =>
+    job.title.toLowerCase().includes(query)
+  );
 
   displayResults(results);
-  
 }
 
-function displayResults(results){
-  const container = document.getElementById('results');
-  container.innerHTML = '';
-  if(results.length === 0){
-    container.innerHTML = '<p>No jobs found.</p>';
+function displayResults(results) {
+  const container = document.getElementById("results");
+  container.innerHTML = "";
+  if (results.length === 0) {
+    container.innerHTML = "<p>No jobs found.</p>";
     return;
   }
-  results.forEach(job => {
-    const div = document.createElement('div');
-    div.className = 'job-card';
+  results.forEach((job) => {
+    const div = document.createElement("div");
+    div.className = "job-card";
     div.innerHTML = `
       <div class="job-title">${job.title}</div>
       <div class="job-info">${job.company} - ${job.location}</div>
@@ -35,6 +34,4 @@ function displayResults(results){
     `;
     container.appendChild(div);
   });
-
-  }
-
+}
